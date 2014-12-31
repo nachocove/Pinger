fea5bd30cb6dee72c82274b70fff68a1c91d94dc
Jan Vilhuber <jan@vilhuber.com>
2014-12-30 17:36:31 -0700

    periodically send something

--- Pinger/exchange.go
+++ Pinger/exchange.go
@@ -3,11 +3,14 @@ package Pinger
 import (
 	"fmt"
 	"sync"
+	"time"
+	"math/rand"
 )
 
 // ExchangeClient A client with type exchange.
 type ExchangeClient struct {
 	client *Client
+	debug bool
 }
 
 // String convert the ExchangeClient structure to something printable
@@ -17,16 +20,38 @@ func (ex *ExchangeClient) String() string {
 
 // Exchangehandler the incoming data handler
 func Exchangehandler(data []byte) {
-	fmt.Println("ExchangeClient received", string(data))
+	//fmt.Println("ExchangeClient received", string(data))
 }
 
+var rng *rand.Rand
+
+func init() {
+	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func randomInt(x, y int) int {
+	return rand.Intn(y-x) + x
+}
+
+func (ex *ExchangeClient) periodicCheck() {
+	data := fmt.Sprintf("Greetings from %s", ex.client.connection.LocalAddr().String())
+
+	for {
+		sleepTime := randomInt(10,30)
+		if ex.debug {
+			fmt.Printf("Sleeping %d\n", sleepTime)
+		}
+		time.Sleep(time.Duration(sleepTime)*time.Second)
+		if ex.debug {
+			fmt.Println("ExchangeClient sending", data)
+		}
+		ex.client.outgoing <- []byte(data)
+	}
+}
 // Listen sets up the exchange client to listen. Most of the hard work is done via the Client.Listen()
 func (ex *ExchangeClient) Listen(wait *sync.WaitGroup) {
 	ex.client.Listen(wait)
-
-	data := "12345"
-	fmt.Println("ExchangeClient sending", data)
-	ex.client.outgoing <- []byte("12345")
+	go ex.periodicCheck()
 }
 
 // TODO This really ought to just be a method/interface thing
@@ -35,5 +60,5 @@ func (ex *ExchangeClient) Listen(wait *sync.WaitGroup) {
 func NewExchangeClient(dialString string, debug bool) *ExchangeClient {
 	client := NewClient(dialString, debug)
 	client.incomingHandler = Exchangehandler
-	return &ExchangeClient{client}
+	return &ExchangeClient{client, debug}
 }
