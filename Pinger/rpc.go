966c2c9000de9149b8fe984c51767f57fc477d1f
Jan Vilhuber <jan@vilhuber.com>
2015-01-20 14:04:10 -0700

    Prepare for some testing code

--- Pinger/rpc.go
+++ Pinger/rpc.go
@@ -9,6 +9,7 @@ import (
 	"net/rpc"
 
 	"github.com/op/go-logging"
+	"runtime"
 )
 
 type BackendPolling int
@@ -34,16 +35,15 @@ const (
 	PollingReplyError = 0
 )
 
-func (t *BackendPolling) Start(args *StartPollArgs, reply *PollingResponse) error {
-	defer recoverCrash()
-	logger.Debug("Received request for %s", args.Device.ClientId)
+func (t *BackendPolling) start(args *StartPollArgs, reply *PollingResponse) error {
+	RpcLogger.Debug("Received request for %s", args.Device.ClientId)
 	replyCode := PollingReplyOK
 	pi, ok := pollMap[args.Device.ClientId]
 	if ok == true {
 		if pi == nil {
 			return errors.New(fmt.Sprintf("Could not find poll item in map: %s", args.Device.ClientId))
 		}
-		logger.Debug("Already polling for %s", args.Device.ClientId)
+		RpcLogger.Debug("Already polling for %s", args.Device.ClientId)
 		reply.Message = "Running"
 		// TODO Check to see if we're still running. Maybe get a status and return it. Maybe some stats?
 		// If we detect any issues with the polling routine for this client, kill it and set pi to nil.
@@ -59,11 +59,10 @@ func (t *BackendPolling) Start(args *StartPollArgs, reply *PollingResponse) erro
 		pingPeriodicity := 5
 		reopenConnection := true
 		debug := false
-		logger := logger
 		var mailserver MailServer
 		switch {
 		case args.Device.MailClientType == "exchange":
-			mailserver = NewExchangeClient(dialString, pingPeriodicity, reopenConnection, nil, 0, debug, logger)
+			mailserver = NewExchangeClient(dialString, pingPeriodicity, reopenConnection, nil, 0, debug, RpcLogger)
 
 		default:
 			panic(fmt.Sprintf("Unknown/unsupported mailserver type %s", args.Device.MailClientType))
@@ -73,22 +72,16 @@ func (t *BackendPolling) Start(args *StartPollArgs, reply *PollingResponse) erro
 			mailServer: mailserver,
 		}
 		pollMap[args.Device.ClientId] = &pi
-		logger.Debug("Starting polling for %s", args.Device.ClientId)
+		RpcLogger.Debug("Starting polling for %s", args.Device.ClientId)
 		reply.Message = "Started"
+		panic("WWWWW")
 	}
 	reply.Code = replyCode
 	return nil
 }
 
-func recoverCrash() {
-	if r := recover(); r != nil {
-		fmt.Println("Crash:", r)
-	}
-}
-
-func (t *BackendPolling) Stop(args *StopPollArgs, reply *PollingResponse) error {
-	defer recoverCrash()
-	logger.Debug("Received request for %s", args.ClientId)
+func (t *BackendPolling) stop(args *StopPollArgs, reply *PollingResponse) error {
+	RpcLogger.Debug("Received request for %s", args.ClientId)
 	replyCode := PollingReplyOK
 	pi, ok := pollMap[args.ClientId]
 	if ok == false {
@@ -106,15 +99,35 @@ func (t *BackendPolling) Stop(args *StopPollArgs, reply *PollingResponse) error 
 	return nil
 }
 
-var logger *logging.Logger
+func recoverCrash() {
+	if err := recover(); err != nil {
+		stack := make([]byte, 8*1024)
+		stack = stack[:runtime.Stack(stack, false)]
+		RpcLogger.Error("Error: %s\n%s", err, stack)
+	}
+}
+func (t *BackendPolling) Start(args *StartPollArgs, reply *PollingResponse) error {
+	defer recoverCrash()
+	return t.start(args, reply)
+}
+
+func (t *BackendPolling) Stop(args *StopPollArgs, reply *PollingResponse) error {
+	defer recoverCrash()
+	return t.stop(args, reply)
+}
+
+var RpcLogger *logging.Logger
+func InitRpc(logger *logging.Logger) {
+	RpcLogger = logger
+}
 
 func StartPollingRPCServer(l *logging.Logger) {
-	logger = l
+	InitRpc(l)
 	pollingAPI := new(BackendPolling)
 	rpc.Register(pollingAPI)
 	rpc.HandleHTTP()
 	rpcConnectString := fmt.Sprintf("%s:%d", "localhost", RPCPort)
-	logger.Info("Starting RPC server on %s", rpcConnectString)
+	RpcLogger.Info("Starting RPC server on %s", rpcConnectString)
 	err := http.ListenAndServe(rpcConnectString, nil)
 	if err != nil {
 		panic(err)
