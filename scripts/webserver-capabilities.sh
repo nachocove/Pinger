3f173a26e99e615a3d34a21b218423fc3c6cdcfc
Jan Vilhuber <jan@vilhuber.com>
2015-02-05 10:10:18 -0700

    make command fail gracefully on anything other than linux

--- scripts/webserver-capabilities.sh
+++ scripts/webserver-capabilities.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ `uname -s` != "Linux" ] ; then
+   echo "This script only works on linux"
+   exit 0
+fi
+
 if [ -z $GOPATH ] ; then
     echo "ERROR: GOPATH must be set"
     exit 1
