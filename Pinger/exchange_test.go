0877dd105a0862db38b40941d2a65199d517c38f
Jan Vilhuber <jan@vilhuber.com>
2015-03-10 12:39:07 -0600

    add exchange tests

---
+++ Pinger/exchange_test.go
@@ -0,0 +1,23 @@
+package Pinger
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewExchangeClient(t *testing.T) {
+	assert := assert.New(t)
+	
+	parent := &MailClientContext{}
+	debug := true
+	
+	ex, err := NewExchangeClient(parent, debug, logger)
+	assert.NoError(err)
+	assert.NotNil(ex)
+	
+	assert.NotNil(ex.parent)
+	assert.Equal(parent, ex.parent)
+	assert.NotNil(ex.incoming)
+	assert.Equal(debug, ex.debug)
+	assert.Equal(logger, ex.logger)
+}
