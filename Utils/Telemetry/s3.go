fbafe19c9a308abf643afb5b32d10b874c9152e7
Jan Vilhuber <jan@vilhuber.com>
2015-03-23 11:02:48 -0600

    remove extraneous debug message

--- Utils/Telemetry/s3.go
+++ Utils/Telemetry/s3.go
@@ -9,7 +9,6 @@ func (writer *TelemetryWriter) pushToS3(fileName string) error {
 		bucket := writer.uploadLocationPrefixUrl.Host
 		srcPath := path.Join(writer.fileLocationPrefix, fileName)
 		destPath := path.Join("/", writer.uploadLocationPrefixUrl.Path, fileName)
-		writer.logger.Printf("Writing file %s to s3://%s%s", srcPath, bucket, destPath)
 		err := writer.awsConfig.PutFile(bucket, srcPath, destPath)
 		if err != nil {
 			return err
