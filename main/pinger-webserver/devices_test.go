fb6feb51f9a1b5ec55c647f7e84930f2647e8bc8
Jan Vilhuber <jan@vilhuber.com>
2015-04-06 11:33:50 -0600

    convert to suite

--- main/pinger-webserver/devices_test.go
+++ main/pinger-webserver/devices_test.go
@@ -5,138 +5,147 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/nachocove/Pinger/Pinger"
 	"github.com/nachocove/Pinger/Utils/Logging"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
 )
 
-var logger *Logging.Logger
-var mx *mux.Router
-var n *negroni.Negroni
-var registerPath string = "/register"
-var fakeUrl string = "http://mypinger.com"
-var fakeRegisterUrl string = fakeUrl + registerPath
-var pingerConfig *Pinger.Configuration
-var config *Pinger.Configuration
-var registerJson string = "{\"ClientContext\": \"12345\", \"DeviceId\": \"NchoDC28E565X072CX46B1XBF205\", \"WaitBeforeUse\": 3000, \"MailServerCredentials\": {\"Username\": \"janv\", \"Password\": \"Password1\"}, \"ClientId\": \"us-east-1:0005d365-c8ea-470f-8a61-a7f44f145efb\", \"Platform\": \"ios\", \"RequestData\": \"AwFqAAANRUgDNjAwAAFJSksDNgABTANFbWFpbAABAUpLAzIAAUwDQ2FsZW5kYXIAAQEBAQ==\", \"PushService\": \"APNS\", \"ResponseTimeout\": 600000, \"ExpectedReply\": \"\", \"PushToken\": \"AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEF\", \"MailServerUrl\": \"https://mail.d2.officeburrito.com/Microsoft-Server-ActiveSync?Cmd=Ping&User=janv@d2.officeburrito.com&DeviceId=NchoDC28E565X072CX46B1XBF205&DeviceType=iPad\", \"HttpHeaders\": {\"MS-ASProtocolVersion\": \"14.1\", \"User-Agent\":\"Apple-iPad3C1/1202.466\", \"Content-Length\":\"52\",\"Content-Type\":\"application/vnd.ms-sync.wbxml\"}, \"NoChangeReply\": \"AwFqAAANRUcDMQABAQ==\", \"Protocol\": \"ActiveSync\", \"OSVersion\": \"8.1\", \"AppBuildVersion\": \"0.9\", \"AppBuildNumber\": \"[dev]janv@nachocove.com\"}"
-var rpcTestPort = 40800
+type devicesTester struct {
+	suite.Suite
+	logger          *Logging.Logger
+	mx              *mux.Router
+	n               *negroni.Negroni
+	registerPath    string
+	fakeUrl         string
+	fakeRegisterUrl string
+	pingerConfig    *Pinger.Configuration
+	config          *Pinger.Configuration
+	registerJson    string
+	rpcTestPort int
+}
 
-func TestMain(m *testing.M) {
-	logger = Logging.InitLogging("unittest", "", Logging.DEBUG, true, Logging.DEBUG, nil, true)
+func (s *devicesTester) SetupSuite() {
+	s.logger = Logging.InitLogging("unittest", "", Logging.DEBUG, true, Logging.DEBUG, nil, true)
+	s.registerPath = "/register"
+	s.fakeUrl = "http://mypinger.com"
+	s.fakeRegisterUrl = s.fakeUrl + s.registerPath
+	s.registerJson = "{\"ClientContext\": \"12345\", \"DeviceId\": \"NchoDC28E565X072CX46B1XBF205\", \"WaitBeforeUse\": 3000, \"MailServerCredentials\": {\"Username\": \"janv\", \"Password\": \"Password1\"}, \"ClientId\": \"us-east-1:0005d365-c8ea-470f-8a61-a7f44f145efb\", \"Platform\": \"ios\", \"RequestData\": \"AwFqAAANRUgDNjAwAAFJSksDNgABTANFbWFpbAABAUpLAzIAAUwDQ2FsZW5kYXIAAQEBAQ==\", \"PushService\": \"APNS\", \"ResponseTimeout\": 600000, \"ExpectedReply\": \"\", \"PushToken\": \"AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEF\", \"MailServerUrl\": \"https://mail.d2.officeburrito.com/Microsoft-Server-ActiveSync?Cmd=Ping&User=janv@d2.officeburrito.com&DeviceId=NchoDC28E565X072CX46B1XBF205&DeviceType=iPad\", \"HttpHeaders\": {\"MS-ASProtocolVersion\": \"14.1\", \"User-Agent\":\"Apple-iPad3C1/1202.466\", \"Content-Length\":\"52\",\"Content-Type\":\"application/vnd.ms-sync.wbxml\"}, \"NoChangeReply\": \"AwFqAAANRUcDMQABAQ==\", \"Protocol\": \"ActiveSync\", \"OSVersion\": \"8.1\", \"AppBuildVersion\": \"0.9\", \"AppBuildNumber\": \"[dev]janv@nachocove.com\"}"
+	s.rpcTestPort = 40800
 
 	rpcConfig := Pinger.RPCServerConfiguration{
 		Protocol: "http",
 		Hostname: "localhost",
-		Port:     rpcTestPort,
+		Port:     s.rpcTestPort,
 	}
-	pingerConfig = Pinger.NewConfiguration()
-	pingerConfig.Db.Type = "sqlite"
-	pingerConfig.Db.Filename = ":memory:"
-	pingerConfig.Rpc = rpcConfig
+	s.pingerConfig = Pinger.NewConfiguration()
+	s.pingerConfig.Db.Type = "sqlite"
+	s.pingerConfig.Db.Filename = ":memory:"
+	s.pingerConfig.Rpc = rpcConfig
 
-	mx = mux.NewRouter()
-	mx.HandleFunc(registerPath, registerDevice)
-	config = Pinger.NewConfiguration()
-	config.Rpc = rpcConfig
-	n = negroni.New(NewContextMiddleWare(&Context{Logger: logger, Config: config}))
-	n.UseHandler(mx)
-
-	go startRpc(pingerConfig)
-
-	os.Exit(m.Run())
+	s.mx = mux.NewRouter()
+	s.mx.HandleFunc(s.registerPath, registerDevice)
+	s.config = Pinger.NewConfiguration()
+	s.config.Rpc = rpcConfig
+	s.n = negroni.New(NewContextMiddleWare(&Context{Logger: s.logger, Config: s.config}))
+	s.n.UseHandler(s.mx)
+	go s.startRpc()
 }
 
-func startRpc(config *Pinger.Configuration) {
-	err := Pinger.StartPollingRPCServer(pingerConfig, true, logger)
+func (s *devicesTester) SetupTest() {
+}
+
+func (s *devicesTester) TearDownTest() {
+}
+
+func (s *devicesTester) startRpc() {
+	err := Pinger.StartPollingRPCServer(s.pingerConfig, true, s.logger)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func TestRegisterGet(t *testing.T) {
-	assert := assert.New(t)
-	req, err := http.NewRequest("GET", fakeRegisterUrl, nil)
-	assert.NoError(err)
-	assert.NotNil(req)
-
-	response := httptest.NewRecorder()
-	assert.NotNil(response)
-	n.ServeHTTP(response, req)
-	assert.Equal(400, response.Code)
-	assert.Contains(response.Body.String(), "UNKNOWN METHOD")
+func TestWebDevices(t *testing.T) {
+	s := new(devicesTester)
+	suite.Run(t, s)
 }
 
-func TestRegisterEncodingFail(t *testing.T) {
-	assert := assert.New(t)
-	req, err := http.NewRequest("POST", fakeRegisterUrl, strings.NewReader(""))
-	assert.NoError(err)
-	assert.NotNil(req)
+func (s *devicesTester) TestRegisterGet() {
+	req, err := http.NewRequest("GET", s.fakeRegisterUrl, nil)
+	s.NoError(err)
+	s.NotNil(req)
 
 	response := httptest.NewRecorder()
-	assert.NotNil(response)
-	n.ServeHTTP(response, req)
-	assert.Equal(400, response.Code)
-	assert.Contains(response.Body.String(), "UNKNOWN Encoding")
+	s.NotNil(response)
+	s.n.ServeHTTP(response, req)
+	s.Equal(400, response.Code)
+	s.Contains(response.Body.String(), "UNKNOWN METHOD")
 }
 
-func TestRegisterContentFail(t *testing.T) {
-	assert := assert.New(t)
-	req, err := http.NewRequest("POST", fakeRegisterUrl, strings.NewReader("{}"))
-	assert.NoError(err)
-	assert.NotNil(req)
+func (s *devicesTester) TestRegisterEncodingFail() {
+	req, err := http.NewRequest("POST", s.fakeRegisterUrl, strings.NewReader(""))
+	s.NoError(err)
+	s.NotNil(req)
+
+	response := httptest.NewRecorder()
+	s.NotNil(response)
+	s.n.ServeHTTP(response, req)
+	s.Equal(400, response.Code)
+	s.Contains(response.Body.String(), "UNKNOWN Encoding")
+}
+
+func (s *devicesTester) TestRegisterContentFail() {
+	req, err := http.NewRequest("POST", s.fakeRegisterUrl, strings.NewReader("{}"))
+	s.NoError(err)
+	s.NotNil(req)
 	req.Header.Add("Content-Type", "application/json")
 
 	response := httptest.NewRecorder()
-	assert.NotNil(response)
-	n.ServeHTTP(response, req)
-	assert.Equal(400, response.Code)
-	assert.Contains(response.Body.String(), "MISSING_REQUIRED_DATA")
+	s.NotNil(response)
+	s.n.ServeHTTP(response, req)
+	s.Equal(400, response.Code)
+	s.Contains(response.Body.String(), "MISSING_REQUIRED_DATA")
 }
 
-func TestRegisterJsonFail(t *testing.T) {
-	assert := assert.New(t)
-	req, err := http.NewRequest("POST", fakeRegisterUrl, strings.NewReader("{"))
-	assert.NoError(err)
-	assert.NotNil(req)
+func (s *devicesTester) TestRegisterJsonFail() {
+	req, err := http.NewRequest("POST", s.fakeRegisterUrl, strings.NewReader("{"))
+	s.NoError(err)
+	s.NotNil(req)
 	req.Header.Add("Content-Type", "application/json")
 
 	response := httptest.NewRecorder()
-	assert.NotNil(response)
-	n.ServeHTTP(response, req)
-	assert.Equal(400, response.Code)
-	assert.Contains(response.Body.String(), "Could not parse json")
+	s.NotNil(response)
+	s.n.ServeHTTP(response, req)
+	s.Equal(400, response.Code)
+	s.Contains(response.Body.String(), "Could not parse json")
 }
 
-func TestRegisterRPCFail(t *testing.T) {
-	assert := assert.New(t)
-	config.Rpc.Port = 10
-	config.Server.TokenAuthKey = "01234567890123456789012345678901"
-	req, err := http.NewRequest("POST", fakeRegisterUrl, strings.NewReader(registerJson))
-	assert.NoError(err)
-	assert.NotNil(req)
+func (s *devicesTester) TestRegisterRPCFail() {
+	s.config.Rpc.Port = 10
+	s.config.Server.TokenAuthKey = "01234567890123456789012345678901"
+	req, err := http.NewRequest("POST", s.fakeRegisterUrl, strings.NewReader(s.registerJson))
+	s.NoError(err)
+	s.NotNil(req)
 	req.Header.Add("Content-Type", "application/json")
 
 	response := httptest.NewRecorder()
-	assert.NotNil(response)
-	n.ServeHTTP(response, req)
-	assert.Equal(500, response.Code)
-	assert.Contains(response.Body.String(), "RPC_SERVER_ERROR")
+	s.NotNil(response)
+	s.n.ServeHTTP(response, req)
+	s.Equal(500, response.Code)
+	s.Contains(response.Body.String(), "RPC_SERVER_ERROR")
 }
 
-//func TestRegisterContentSuccess(t *testing.T) {
-//	assert := assert.New(t)
+//func (s *devicesTester) TestRegisterContentSuccess() {
 //  config.Rpc.Port = rpcTestPort
 //	req, err := http.NewRequest("POST", fakeRegisterUrl, strings.NewReader(registerJson))
-//	assert.NoError(err)
-//	assert.NotNil(req)
+//	s.NoError(err)
+//	s.NotNil(req)
 //	req.Header.Add("Content-Type", "application/json")
 //
 //	response := httptest.NewRecorder()
-//	assert.NotNil(response)
-//	n.ServeHTTP(response, req)
-//	assert.Equal(200, response.Code)
-//	assert.Contains(response.Body.String(), "\"Status\":\"OK\"")
+//	s.NotNil(response)
+//	s.n.ServeHTTP(response, req)
+//	s.Equal(200, response.Code)
+//	s.Contains(response.Body.String(), "\"Status\":\"OK\"")
 //}
