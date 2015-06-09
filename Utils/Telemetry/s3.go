8eb3512c5e489a6a9e221bfa7a6a0f00be5d1d89
azimnacho <azimo@nachocove.com>
2015-06-08 18:11:30 -0700

    updated pinger to use  T3 style logging

--- Utils/Telemetry/s3.go
+++ Utils/Telemetry/s3.go
@@ -8,7 +8,8 @@ func (writer *TelemetryWriter) pushToS3(fileName string) error {
 	if writer.uploadLocationPrefixUrl != nil {
 		bucket := writer.uploadLocationPrefixUrl.Host
 		srcPath := path.Join(writer.fileLocationPrefix, fileName)
-		destPath := path.Join("/", writer.uploadLocationPrefixUrl.Path, fileName)
+		datePrefix := fileName[5:13]
+		destPath := path.Join("/", writer.uploadLocationPrefixUrl.Path, "/", datePrefix, "/", fileName)
 		if writer.debug {
 			writer.logger.Printf("Uploading %s to s3://%s%s", srcPath, bucket, destPath)
 		}
